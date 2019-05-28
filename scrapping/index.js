8d4221565c1ddb3c212b2aac2e7e7b025ae6c7de
Gabriel <gabriel_silvapaiva@hotmail.com>
2019-05-28 20:26:25 -0300

    Adicionando link da publicacao

--- scrapping/index.js
+++ scrapping/index.js
@@ -8,11 +8,15 @@ const parseBody = (body) => {
     const $  = cheerio.load(body);
     const data = [];
     $(".card__data").each((i,element) => {
-        const cardData = cheerio.load(element);
-        let title = cardData(".card__title > a").text().replace(/\n/g,'').trim();   
-        let description = cardData(".card__excerpt").text().replace(/\n/g,'').trim();
-        let authors = cardData(".card__authors > span > a").text();
-        data.push({title, description, authors});
+        if(i > 5){
+            const cardData = cheerio.load(element);
+            let title = cardData(".card__title > a").text().replace(/\n/g,'').trim();   
+            let link = 'https://www.infoq.com' + cardData(".card__title > a").attr('href');
+            let description = cardData(".card__excerpt").text().replace(/\n/g,'').trim();
+            let authors = cardData(".card__authors > span > a").text();
+            data.push({title, link, description, authors});
+        }
+        
     });
     return data;
 }
